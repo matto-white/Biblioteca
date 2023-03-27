ad5d89cd9496e057f2030e669e5032f89e48fa8b
matto-white <18dragon.mb@gmail.com>
2023-03-27 20:30:00 +0200

    27/03/2023 20:28

--- src/test/java/BibliotecaTest.java
+++ src/test/java/BibliotecaTest.java
@@ -10,6 +10,8 @@ class BibliotecaTest {
 
     Biblioteca biblioteca;
     Libro libro;
+    Tesserati tesserati;
+    Persona persona;
     ArrayList<Integer> indiciCercaTitoloCrypto;
     ArrayList<Integer> indiciCercaTitoloGhiaccio;
     ArrayList<Integer> indiciCercaTitoloNull;
@@ -18,12 +20,25 @@ class BibliotecaTest {
     ArrayList<Integer> indiciCercaCodice1234;
     ArrayList<Integer> indiciCercaCodice1235;
     ArrayList<Integer> indiciCercaCodiceNull;
+    ArrayList<Integer> indiciCercaNomeMattia;
+    ArrayList<Integer> indiciCercaNomeDavide;
+    ArrayList<Integer> indiciCercaNomeNull;
+    ArrayList<Integer> indiciCercaCognomeBianco;
+    ArrayList<Integer> indiciCercaCognomeGiuliani;
+    ArrayList<Integer> indiciCercaCognomeNull;
+    ArrayList<Integer> indiciCercaMatricola0987AB;
+    ArrayList<Integer> indiciCercaMatricola1234AB;
+    ArrayList<Integer> indiciCercaMatricolaNull;
+
+
 
     @BeforeEach
     void setUp(){
         biblioteca = new Biblioteca();
         biblioteca.add(libro = new Libro("Crypto", "Dan Brown", "1234"));
+        biblioteca.addcodici("1234");
         biblioteca.add(libro = new Libro("I segreti del ghiaccio", "Dan Brown", "1235"));
+        biblioteca.addcodici("1235");
 
         indiciCercaTitoloCrypto = new ArrayList<Integer>();
         indiciCercaTitoloCrypto.add(0);
@@ -42,6 +57,29 @@ class BibliotecaTest {
         indiciCercaCodice1235.add(1);
         indiciCercaCodiceNull = new ArrayList<Integer>();
 
+
+        tesserati = new Tesserati();
+        tesserati.add(persona = new Persona("Mattia", "Bianco", "0987AB"));
+        tesserati.addmatricole("0987AB");
+        tesserati.add(persona = new Persona("Davide", "Giuliani", "1234AB"));
+
+        indiciCercaNomeMattia = new ArrayList<Integer>();
+        indiciCercaNomeMattia.add(0);
+        indiciCercaNomeDavide = new ArrayList<Integer>();
+        indiciCercaNomeDavide.add(1);
+        indiciCercaNomeNull = new ArrayList<Integer>();
+
+        indiciCercaCognomeBianco = new ArrayList<Integer>();
+        indiciCercaCognomeBianco.add(0);
+        indiciCercaCognomeGiuliani = new ArrayList<Integer>();
+        indiciCercaCognomeGiuliani.add(1);
+        indiciCercaCognomeNull = new ArrayList<Integer>();
+
+        indiciCercaMatricola0987AB = new ArrayList<Integer>();
+        indiciCercaMatricola0987AB.add(0);
+        indiciCercaMatricola1234AB = new ArrayList<Integer>();
+        indiciCercaMatricola1234AB.add(1);
+        indiciCercaMatricolaNull = new ArrayList<Integer>();
     }
     @Test
     void testCercaTitolo() {
@@ -77,6 +115,52 @@ class BibliotecaTest {
     }
 
     @Test
-    void testGiausato() {
+    void testGiausatoCodice() {
+        assertTrue(biblioteca.giausato("1234"));
+        assertTrue(biblioteca.giausato("1235"));
+        assertFalse(biblioteca.giausato("1236"));
+    }
+
+    @Test
+    void testCercaNome(){
+        assertEquals(indiciCercaNomeMattia, tesserati.cerca("nome", "Mattia"));
+        assertEquals(indiciCercaNomeMattia.size(), tesserati.cerca("nome", "Mattia").size());
+
+        assertEquals(indiciCercaNomeDavide, tesserati.cerca("nome", "Davide"));
+        assertEquals(indiciCercaNomeDavide.size(), tesserati.cerca("nome", "Davide").size());
+
+        assertEquals(indiciCercaNomeNull, tesserati.cerca("nome", "a"));
+        assertEquals(indiciCercaNomeNull.size(), tesserati.cerca("nome", "a").size());
+    }
+
+    @Test
+    void testCercaCognome(){
+        assertEquals(indiciCercaCognomeBianco, tesserati.cerca("cognome", "Bianco"));
+        assertEquals(indiciCercaCognomeBianco.size(), tesserati.cerca("cognome", "Bianco").size());
+
+        assertEquals(indiciCercaCognomeGiuliani, tesserati.cerca("cognome", "Giuliani"));
+        assertEquals(indiciCercaCognomeGiuliani.size(), tesserati.cerca("cognome", "Giuliani").size());
+
+        assertEquals(indiciCercaCognomeNull, tesserati.cerca("cognome", "a"));
+        assertEquals(indiciCercaCognomeNull.size(), tesserati.cerca("cognome", "a").size());
+    }
+
+    @Test
+    void testCercaMatricola(){
+        assertEquals(indiciCercaMatricola0987AB, tesserati.cerca("matricola", "0987AB"));
+        assertEquals(indiciCercaMatricola0987AB.size(), tesserati.cerca("matricola", "0987AB").size());
+
+        assertEquals(indiciCercaMatricola1234AB, tesserati.cerca("matricola", "1234AB"));
+        assertEquals(indiciCercaMatricola1234AB.size(), tesserati.cerca("matricola", "1234AB").size());
+
+        assertEquals(indiciCercaMatricolaNull, tesserati.cerca("matricola", "a"));
+        assertEquals(indiciCercaMatricolaNull.size(), tesserati.cerca("matricola", "a").size());
+    }
+
+    @Test
+    void testGiausatoMatricola() {
+        assertTrue(tesserati.giausato("0987AB"));
+        assertTrue(tesserati.giausato("1234AB"));
+        assertFalse(tesserati.giausato("1236AB"));
     }
 }
